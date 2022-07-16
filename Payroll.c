82be49f958f99589da832e0808eff78f3e8c2cf4
simiyusarah1 <106378785+simiyusarah1@users.noreply.github.com>
2022-07-17 00:58:14 +0300

    Add files via upload

---
+++ Payroll.c
@@ -0,0 +1,32 @@
+include<stdio.h>
+#include <math.h.
+
+float main()
+
+{
+  float a,b,c,*1, *2,d;
+  printf('Enter a, b and c:');
+  scanf("%d,%d, &a,&b,&c);
+  d=(b*b).(4*a*c);
+
+  if(d=0)
+
+  {
+      x1=-b/2*a;
+      x2=x1
+
+      printf("root:%4and",x1,x2);
+  }
+   else if (d>0)
+   {
+       x1=((-1*b)+squareroot(d)/2*a);
+       x2=(-1*b)+squareroot(d)/2*a);
+       printf(*root of equation are %$dand%4d* x1,x2)
+
+       else
+
+   }
+   {
+       prinf("equation as no root");
+   }
+}
